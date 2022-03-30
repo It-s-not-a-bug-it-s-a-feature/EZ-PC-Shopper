fda0ef6ce8452349c32c9e1960222dac7b312329
ELIZABETH BEALE <EGBEALE@GMAIL.COM>
2022-03-29 17:09:32 -0700

    finished setting storage

--- js/storage.js
+++ js/storage.js
@@ -3,10 +3,12 @@
 let configuredComputer = Object();
 
 function writeToStorage(computerCategory) {
-  let stringifiedThing = JSON.strifigy(configuredComputer);
+  let stringifiedThing = JSON.stringify(configuredComputer);
   localStorage.setItem(computerCategory, stringifiedThing);
-
 }
+
+// Remove this variable assignment when it's no longer needed ****
 let computerCategory = 'productivity';
 
+// *** Put this into an event handler and set computerCategory!! *****
 writeToStorage(computerCategory);
