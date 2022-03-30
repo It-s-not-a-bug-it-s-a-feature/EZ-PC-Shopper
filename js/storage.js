46b326d3fca836466f81c9f9dcbcbf69f108cdeb
ELIZABETH BEALE <EGBEALE@GMAIL.COM>
2022-03-29 17:36:09 -0700

    implemented getStorage

--- js/storage.js
+++ js/storage.js
@@ -12,3 +12,21 @@ let computerCategory = 'productivity';
 
 // *** Put this into an event handler and set computerCategory!! *****
 writeToStorage(computerCategory);
+
+function readFromStorage(computerCategory) {
+  let retrievedThing = localStorage.getItem(computerCategory);
+  let parsedThing = JSON.parse(retrievedThing);
+  return parsedThing;
+}
+
+// **** here's an EXAMPLE of how to write + read from storage for coders ****
+
+// configuredComputer = {
+//   test: 'i am a test'
+// };
+
+// writeToStorage('testing');
+
+// let thingFromStorage = readFromStorage('testing');
+// console.log(thingFromStorage);
+
